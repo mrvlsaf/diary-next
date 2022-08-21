f357613c9e242adca521cc5530c86e0b32d322ea
Devraj Singh <dsdevrajsingh55555@gmail.com>
2022-08-21 12:37:34 +0530

    setting up the base structure

---
+++ components/layout/MainNavigation.js
@@ -0,0 +1,22 @@
+import classes from './MainNavigation.module.css'
+import Link from 'next/link'
+
+function MainNavigation() {
+    return (
+        <header className={classes.header}>
+            <div className={classes.logo}>Diary Next</div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href='/'>Create Journal</Link>
+                    </li>
+                    <li>
+                        <Link href='/journals'>All Journals</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default MainNavigation;
---
+++ pages/journals.js
@@ -0,0 +1,9 @@
+function Journals () {
+    return (
+        <>
+            <h1>All the journals will be listed here</h1>
+        </>
+    )
+}
+
+export default Journals
